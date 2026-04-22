0098b387ae1c02b266b39bb6753ba1be75cda907
GuYuLong <1327417509@qq.com>
2026-04-22 12:48:57 +0800

    更新部署脚本网站地址

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 yarn docs:build
-scp -i /Users/guyulong/.ssh/lengmang.pem -r ./docs/.vuepress/dist/** root@www.lengmang.net:~/nginx/app/root
+scp -i /Users/guyulong/.ssh/lengmang.pem -r ./docs/.vuepress/dist/** root@www.lengmang.cn:~/nginx/app/root
